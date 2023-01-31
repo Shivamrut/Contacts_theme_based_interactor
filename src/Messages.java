ce2d65f0506569f6816ab32978fbb946bb84af66
maxus_red <incineroaraditya@gmail.com>
2023-01-31 14:28:50 +0530

    Created Messages class

---
+++ src/Messages.java
@@ -0,0 +1,38 @@
+public class Messages {
+    private String text;
+    private String recipient;
+    private int id;
+
+    public Messages(String text, String recipient, int id) {
+        this.text = text;
+        this.recipient = recipient;
+        this.id = id;
+    }
+
+    public void getDetails(){
+        System.out.println("Contact name: "+ recipient+"\nContact id: "+id+ "\n"+text+"\n");
+    }
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
